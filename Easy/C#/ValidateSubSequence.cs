956a7de75bee30faa209e14450b3bb7f5c0eb2f6
Khaled El Hamzi <contact@elhamzi.me>
2021-10-24 20:31:19 +0200

    Implement array subsequence c#.

---
+++ Easy/C#/ValidateSubSequence.cs
@@ -0,0 +1,35 @@
+public class ValidateSubSequence
+{
+
+  // O(n) | O(1)
+  public static bool IsValidSubsequence1(List<int> array, List<int> sequence)
+  {
+    int arrayIndex = 0;
+    int sequenceIndex = 0;
+    while (arrayIndex < array.Count() && sequenceIndex < array.Count())
+    {
+      if (array[arrayIndex] == sequence[sequenceIndex])
+        sequenceIndex++;
+
+      arrayIndex++;
+    }
+
+    return sequenceIndex == sequence.Count();
+  }
+
+  // O(n) | O(1)
+  public static bool IsValidSubsequence2(List<int> array, List<int> sequence)
+  {
+    int sequenceIndex = 0;
+    foreach (int number in array)
+    {
+      if (sequenceIndex == sequence.Count())
+        break;
+      if (sequence[sequenceIndex] == number)
+        sequenceIndex++;
+    }
+
+    return sequenceIndex == sequence.Count();
+  }
+
+}
