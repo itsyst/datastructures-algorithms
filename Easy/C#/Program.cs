35aeba1dd4602ff37b576e5fd47b9745bc8c94ad
Khaled El Hamzi <contact@elhamzi.me>
2021-11-03 12:44:50 +0100

    Implement sorted squared array c#.

--- Easy/C#/Program.cs
+++ Easy/C#/Program.cs
@@ -1,26 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more informationusing System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      // Two Number Sum
-      // int[] array = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
-      // int targetSum = 10;
-      // int[] result = TwoNumbersSum.TwoNumberSum3(array, targetSum);
-      // Console.WriteLine("[{0}]", string.Join(", ", result));
+        public static void Main(string[] args)
+        {
+            // Two Number Sum
+            // int[] array = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
+            // int targetSum = 10;
+            // int[] result = TwoNumbersSum.TwoNumberSum3(array, targetSum);
+            // Console.WriteLine("[{0}]", string.Join(", ", result));
 
-      // Validate Sub Sequence
-      List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
-      List<int> sequence = new List<int> { 1, 6, -1, 10 };
-      Boolean result = ValidateSubSequence.IsValidSubsequence2(array, sequence);
-      Console.WriteLine(result);
+            // Validate Sub Sequence
+            // List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+            // List<int> sequence = new List<int> { 1, 6, -1, 10 };
+            // Boolean result = ValidateSubSequence.IsValidSubsequence2(array, sequence);
+            // Console.WriteLine(result);
+
+            // Sorted Squared Array
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] result = SortedSquaredArrays.SortedSquaredArray3(array);
+            Console.WriteLine("[{0}]", string.Join(", ", result));
+        }
     }
-  }
 }
 
 
---
+++ Easy/C#/SortedSquaredArrays.cs
@@ -0,0 +1,52 @@
+class SortedSquaredArrays
+{
+
+    // O(nlogn) time | O(n) space
+    public static int[] SortedSquaredArray1(int[] array)
+    {
+        int[] squared = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+            squared[i] = (int)Math.Pow(array[i], 2);
+
+        Array.Sort(squared);
+
+        return squared;
+    }
+
+
+    // O(n) time | O(n) space
+    public static int[] SortedSquaredArray2(int[] array)
+    {
+        int[] squared = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+            squared[i] = (int)Math.Pow(array[i], 2);
+
+        Array.Sort(squared);
+
+        return squared;
+    }
+
+    // O(n) time | O(n) space - Where n is the length of the input array.
+    public static int[] SortedSquaredArray3(int[] array)
+    {
+        int[] squared = new int[array.Length];
+        int start = 0;
+        int end = array.Length - 1;
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (array[start] < array[end])
+            {
+                squared[i] = (int)Math.Pow(array[end], 2);
+                end--;
+            }
+            else
+            {
+                squared[i] = (int)Math.Pow(array[start], 2);
+                start++;
+            }
+        }
+
+        return squared;
+    }
+}
+
