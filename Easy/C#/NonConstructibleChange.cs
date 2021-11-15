7317354b81a152b5a4db1e938981dd1de64c4ebb
Khaled El Hamzi <contact@elhamzi.me>
2021-11-15 23:35:58 +0100

    Implemrnt non construct change coins c#.

---
+++ Easy/C#/NonConstructibleChange.cs
@@ -0,0 +1,20 @@
+class NonConstructibleChange
+{
+
+    // O(nlogn) time | O(1) space
+    public static int NonConstructibleChanges(int[] coins)
+    {
+        Array.Sort(coins);
+        int currentCoinChange = 0;
+        foreach (var coin in coins)
+        {
+            if (coin > currentCoinChange +1)
+                return currentCoinChange += 1;
+
+            currentCoinChange += coin;
+        }
+
+        return currentCoinChange + 1;
+    }
+
+}
--- Easy/C#/Program.cs
+++ Easy/C#/Program.cs
@@ -7,24 +7,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
         public static void Main(string[] args)
         {
             // Two Number Sum
-            // int[] array = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
-            // int targetSum = 10;
-            // int[] result = TwoNumbersSum.TwoNumberSum3(array, targetSum);
-            // Console.WriteLine("[{0}]", string.Join(", ", result));
+            /* int[] array = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
+               int targetSum = 10;
+               int[] result = TwoNumbersSum.TwoNumberSum3(array, targetSum);
+               Console.WriteLine("[{0}]", string.Join(", ", result));*/
 
             // Validate Sub Sequence
-            // List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
-            // List<int> sequence = new List<int> { 1, 6, -1, 10 };
-            // Boolean result = ValidateSubSequence.IsValidSubsequence2(array, sequence);
-            // Console.WriteLine(result);
+            /* List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+                List<int> sequence = new List<int> { 1, 6, -1, 10 };
+               Boolean result = ValidateSubSequence.IsValidSubsequence2(array, sequence);
+               Console.WriteLine(result);*/
 
             // Sorted Squared Array
-            // int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            // int[] result = SortedSquaredArrays.SortedSquaredArray3(array);
-            // Console.WriteLine("[{0}]", string.Join(", ", result));
+            /* int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+               int[] result = SortedSquaredArrays.SortedSquaredArray3(array);
+               Console.WriteLine("[{0}]", string.Join(", ", result));*/
 
             // Tournament Winner
-            List<List<string>> competitions = new List<List<string>> {
+            /*List<List<string>> competitions = new List<List<string>> {
                 new List<string>{"HTML", "Java" },
                 new List<string>{"Java", "Python" },
                 new List<string>{"Python", "HTML" },
@@ -39,6 +39,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             List<int> results = new List<int> { 0, 1, 1, 1, 0, 1, 0, 1, 1, 0 };
             string result = TournamentWinner.TournamentWinner1(competitions, results);
+            Console.WriteLine("{0}", string.Join(", ", result));*/
+
+            // Non - Constructible change
+            int[] coins = new int[] { 1, 2, 5 };
+            int result = NonConstructibleChange.NonConstructibleChanges(coins);
             Console.WriteLine("{0}", string.Join(", ", result));
 
         }
