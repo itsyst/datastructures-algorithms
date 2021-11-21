45d64e0a8d595bb5735c48109b9b128bf6f54cfa
Khaled El Hamzi <contact@elhamzi.me>
2021-11-22 00:04:16 +0100

    Implement binary tree node's depth sum Javascript.

---
+++ Easy/JAVASCRIPT/NodesDepthSum.js
@@ -0,0 +1,33 @@
+function nodesDepthSum(root) {
+    if (root === null) return;
+
+    return nodesDepthSumHelper(root, 0);
+}
+
+function nodesDepthSumHelper(root, height) {
+    if (root === null) return 0;
+
+    return height + nodesDepthSumHelper(root.left, height + 1) + nodesDepthSumHelper(root.right, height + 1);
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const root = new Node(10);
+root.left = new Node(5);
+root.left.left = new Node(2);
+root.left.left.left = new Node(1);
+root.left.right = new Node(5);
+root.right = new Node(15);
+root.right.left = new Node(13);
+root.right.left.right = new Node(14);
+root.right.right = new Node(22);
+
+
+const result = nodesDepthSum(root);
+console.log(result);
