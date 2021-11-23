f2ddab6c3d8b9ed174892c3815e2a1f38f01c9ef
Khaled El Hamzi <contact@elhamzi.me>
2021-11-23 17:34:23 +0100

    Prepare for a higher difficulty level.

---
+++ Hard/C#/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more informationusing System;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
+
