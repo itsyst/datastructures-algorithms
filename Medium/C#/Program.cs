abc74f74381d362233158dd36722e8a2629f1208
Khaled El Hamzi <contact@elhamzi.me>
2021-12-10 16:02:54 +0100

    Higher-level data structures.

--- Medium/C#/Program.cs
+++ Medium/C#/Program.cs
@@ -6,9 +6,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
         public static void Main(string[] args)
         {
             // Three Number Sum
-            int[] array = new int[] { 7, 6, 4, -1, 1, 2 };
-            int target = 16;
-            var result = ForNumberSum.ForNumbersSum3(array, target);
+            int[] array = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            int target = 0;
+            var result = ThreeNumberSum.ThreeNumbersSum3(array, target);
             foreach (var item in result)
             {
                 // item.ToList().ForEach(Console.Write);
