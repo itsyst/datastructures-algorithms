c312d27189f279d7bfd990eaa7191ba44822a8d4
Khaled El Hamzi <contact@elhamzi.me>
2021-12-11 23:40:02 +0100

    Implement smallest difference algorithm C#.

--- Medium/C#/Program.cs
+++ Medium/C#/Program.cs
@@ -6,15 +6,21 @@ namespace MyApp // Note: actual namespace depends on the project name.
         public static void Main(string[] args)
         {
             // Three Number Sum
-            int[] array = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
-            int target = 0;
-            var result = ThreeNumberSum.ThreeNumbersSum3(array, target);
-            foreach (var item in result)
-            {
-                // item.ToList().ForEach(Console.Write);
-                // Array.ForEach(item, Console.Write);
-                Console.WriteLine("[" + "{0}", string.Join(", ", item)+ "]");
-            }
+            // int[] array = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            // int target = 0;
+            // var result = ThreeNumberSum.ThreeNumbersSum3(array, target);
+            // foreach (var item in result)
+            // {
+            //     // item.ToList().ForEach(Console.Write);
+            //     // Array.ForEach(item, Console.Write);
+            //     Console.WriteLine("[" + "{0}", string.Join(", ", item)+ "]");
+            // }
+
+            // Get the Smallest Diffrance
+            int[] one = new int[] {10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123, 530 };
+            int[] two = new int[] { -1441, -124, -25, 1014, 1500, 660, 410, 245, 530 };
+            int[] result = SmallestDifference.SmallestDifferences2(one, two);
+            Console.WriteLine("{0}", string.Join(", ", result));
         }
     }
 }
---
+++ Medium/C#/SmallestDifference.cs
@@ -0,0 +1,53 @@
+class SmallestDifference
+{
+    // O(n^2) time | O(1) space
+    public static int[] SmallestDifferences1(int[] arrayOne, int[] arrayTwo)
+    {
+        int[] smallestDiffPair = new int[2];
+        int smallestDiff = Int32.MaxValue;
+
+        for (int i = 0; i < arrayOne.Length; i++)
+            for (int j = 0; j < arrayTwo.Length; j++)
+            {
+                int difference = Math.Abs(arrayOne[i] - arrayTwo[j]);
+                if (difference < smallestDiff)
+                {
+                    smallestDiff = difference;
+                    smallestDiffPair[0] = arrayOne[i];
+                    smallestDiffPair[1] = arrayTwo[j];
+                }
+            }
+
+        return smallestDiffPair;
+    }
+
+    // O(nlog(n) +mlog(m)) time | O(1) space
+    public static int[] SmallestDifferences2(int[] arrayOne, int[] arrayTwo)
+    {
+        Array.Sort(arrayOne);
+        Array.Sort(arrayTwo);
+
+        int[] smallestDiffPair = new int[2];
+        int smallestDiff = Int32.MaxValue;
+
+        int arrayOneIndex = 0;
+        int arrayTwoIndex = 0;
+
+        while (arrayOneIndex < arrayOne.Length && arrayTwoIndex < arrayTwo.Length)
+        {
+            int difference = Math.Abs(arrayOne[arrayOneIndex] - arrayTwo[arrayTwoIndex]);
+            if (difference < smallestDiff)
+            {
+                smallestDiff = difference;
+                smallestDiffPair[0] = arrayOne[arrayOneIndex];
+                smallestDiffPair[1] = arrayTwo[arrayTwoIndex];
+            }
+            if (arrayOne[arrayOneIndex] < arrayTwo[arrayTwoIndex])
+                arrayOneIndex++;
+            else
+                arrayTwoIndex++;
+        }
+ 
+        return smallestDiffPair;
+    }
+}
