5f1cd1dd61d760252fc14c91d03af51ab04184d6
Khaled El Hamzi <contact@elhamzi.me>
2023-05-06 18:11:56 +0000

    Implement merge binary trees. Given two binary trees merge them and return the resulting tree. tf two nodes overlap during the merger then sum the values, otherwise use the existing node.

---
+++ Medium/C#/MergeBinaryTrees.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class MergeBinaryTree
+{
+    // This is an input class. Do not edit.
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree left = null;
+        public BinaryTree right = null;
+
+        public BinaryTree(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    public BinaryTree MergeBinaryTrees(BinaryTree tree1, BinaryTree tree2)
+    {
+        if (tree1 == null)
+        {
+            return tree2;
+        }
+
+        if (tree2 == null)
+        {
+            return tree1;
+        }
+
+        BinaryTree mergedTree = new BinaryTree(tree1.value + tree2.value);
+        mergedTree.left = MergeBinaryTrees(tree1.left, tree2.left);
+        mergedTree.right = MergeBinaryTrees(tree1.right, tree2.right);
+
+        return mergedTree;
+    }
+}
