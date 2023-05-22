99d856641e1575d651818f2df9c9cec4da4032ed
Khaled El Hamzi <contact@elhamzi.me>
2023-05-22 19:29:34 +0000

    Implement water areas csharp. Function that returns the surface area of the water trapped between the pillars viewed from the front .

---
+++ Hard/C#/WaterAreas.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class Program
+{
+    public static int WaterArea(int[] heights)
+    {
+        int[] maxLeftHeights = new int[heights.Length];
+        int[] maxRightHeights = new int[heights.Length];
+
+        int maxLeftHeight = 0;
+        for (int i = 0; i < heights.Length; i++)
+        {
+            maxLeftHeights[i] = maxLeftHeight;
+            maxLeftHeight = Math.Max(maxLeftHeight, heights[i]);
+        }
+
+        int maxRightHeight = 0;
+        for (int i = heights.Length - 1; i >= 0; i--)
+        {
+            maxRightHeights[i] = maxRightHeight;
+            maxRightHeight = Math.Max(maxRightHeight, heights[i]);
+        }
+
+        int waterArea = 0;
+        for (int i = 0; i < heights.Length; i++)
+        {
+            int minHeight = Math.Min(maxLeftHeights[i], maxRightHeights[i]);
+            if (minHeight > heights[i])
+            {
+                waterArea += minHeight - heights[i];
+            }
+        }
+
+        return waterArea;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] heights = { 0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2 };
+
+        int surfaceArea = WaterArea(heights);
+        Console.WriteLine("Surface Area of Water: " + surfaceArea);
+    }
+}
